956a6ae1aefffd18849b01a8f17382e2e94f7a81
A. Belard <angebelard@Gmail.com>
2022-01-26 21:57:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+GREY = (0.78, 0.78, 0.78) #uninfected
+RED = (0.96, 0.15, 0.15) #infected
+GREEN = (0, 0.86, 0.03)  #recovered
+BLACK = (0, 0, 0)  #dead
+
+COVID19_PARAMS = {
+    "r0": 2.28,
+    "incubation": 5,
+    "percent_mild": 0.8,
+    "mild_recovery": (7, 14),
+    "percent_severe": 0.2,
+    "severe_recovery": (21, 42),
+    "severe_death": (14, 56),
+    "fatality_rate": 0.034,
+    "serial_interval": 7
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Virus:
+    def __init__(self, params):
+        self.fig = plt.figure()
+        self.axes = self.fig.add_subplot(111, projection="polar")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
